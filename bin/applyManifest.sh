1642ab08c96cb51473c648194cf984f5304c0133
Felipe Vilarinho <fvilarin@akamai.com>
2025-05-21 12:18:18 -0300

    Initial commit.

--- bin/applyManifest.sh
+++ bin/applyManifest.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# Checks the dependencies of this script.
 function checkDependencies() {
   if [ -z "$KUBECTL_CMD" ]; then
     echo "kubectl is not installed!"
@@ -20,16 +21,19 @@ function checkDependencies() {
   fi
 }
 
+# Prepares the environment to execute this script.
 function prepareToExecute() {
   source ../functions.sh
 }
 
+# Applies the resources required by the automation.
 function apply() {
   if [ -n "$MANIFEST_FILENAME" ] && [ -f "$MANIFEST_FILENAME" ]; then
     $KUBECTL_CMD apply -f "$MANIFEST_FILENAME"
   fi
 }
 
+# Main function.
 function main() {
   prepareToExecute
   checkDependencies

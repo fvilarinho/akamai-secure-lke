e48c617a01c8d6375fea6afc5d7f4bde6420f0ba
fvilarinho <fvilarinho@gmail.com>
2025-05-22 19:43:34 -0300

    Initial commit.

--- iac/main.tf
+++ iac/main.tf
@@ -2,11 +2,6 @@
 terraform {
   # Required providers.
   required_providers {
-    linode = {
-      source = "linode/linode"
-      version = "2.34.0"
-    }
-
     null = {
       source = "hashicorp/null"
       version = "3.2.3"
